50c9b0d5645b6f1d004aa09453cf119cccf38317
Huy Mai <quochuy0703@gmail.com>
2022-05-25 21:54:25 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
+import React, { Component } from "react";
 
-function App() {
-  return <Menu />;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return <Menu dishes={this.state.dishes} />;
+  }
 }
 
 export default App;
