7b94aa03785ab95c9eda912252ed647f6e25a19f
Huy Mai <quochuy0703@gmail.com>
2022-05-25 21:40:52 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+  return <Menu />;
 }
 
 export default App;
