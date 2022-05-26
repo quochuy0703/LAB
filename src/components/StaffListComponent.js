1cda48c2d5b07a481f8525fbcc2401dfa250c2dc
Huy Mai <quochuy0703@gmail.com>
2022-05-26 23:41:49 +0700

    ASM1_commit_6

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -15,29 +15,32 @@ class StaffList extends Component {
     console.log(item);
     if (item != null) {
       return (
-        <Card>
-          <div>
-            <div className="row">
-              <div className="text-center">
-                <img className="avatar" src={item.image} />
-                <h4>Họ và Tên: {item.name}</h4>
+        <div className="my-5">
+          <h2 className="text-center">Chi tiết nhân viên</h2>
+          <Card>
+            <div>
+              <div className="row">
+                <div className="text-center">
+                  <img className="avatar" src={item.image} />
+                  <h4>Họ và Tên: {item.name}</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-6">
+                  <p>Ngày sinh: {dateFormat(item.doB, "dd/mm/yyyy")}</p>
+                  <p>
+                    Ngày vào công ty: {dateFormat(item.startDate, "dd/mm/yyyy")}
+                  </p>
+                  <p>Phòng ban: {item.department.name}</p>
+                </div>
+                <div className="col-6">
+                  <p>Số ngày nghỉ còn lại: {item.annualLeave}</p>
+                  <p>Số ngày làm thêm: {item.overTime}</p>
+                </div>
               </div>
             </div>
-            <div className="row">
-              <div className="col-6">
-                <p>Ngày sinh: {dateFormat(item.doB, "dd/mm/yyyy")}</p>
-                <p>
-                  Ngày vào công ty: {dateFormat(item.startDate, "dd/mm/yyyy")}
-                </p>
-                <p>Phòng ban: {item.department.name}</p>
-              </div>
-              <div className="col-6">
-                <p>Số ngày nghỉ còn lại: {item.annualLeave}</p>
-                <p>Số ngày làm thêm: {item.overTime}</p>
-              </div>
-            </div>
-          </div>
-        </Card>
+          </Card>
+        </div>
       );
     } else {
       return <div>Bấm vào tên nhân viên để xem thông tin</div>;
