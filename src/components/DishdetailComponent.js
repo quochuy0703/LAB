4c082809367067dcf38c2550190a7edb8e1e3d50
Huy Mai <quochuy0703@gmail.com>
2022-06-08 22:04:52 +0700

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -10,6 +10,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import CommentForm from "./CommentForm";
+import { Loading } from "./LoadingComponent";
 
 // class DishDetail extends Component {
 //   constructor(props) {
@@ -86,7 +87,23 @@ function RenderComments({ comments, addComment, dishId }) {
 }
 
 function DishDetail(props) {
-  if (props.dish != null) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
